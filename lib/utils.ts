94477726bf4b0687efc090a3d6a29e50ba8bd16a
agkayster <ejike.chiboka@gmail.com>
2024-06-12 22:10:04 +0100

    Implement shadcn-ui form, button, input and label

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const authFormSchema = () =>
+	z.object({
+		firstName: z.string().min(3),
+		lastName: z.string().min(3),
+		amount: z.string().min(2),
+		email: z.string().email(),
+	});
